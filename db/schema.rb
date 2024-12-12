e86dc2a4639b7ffda3e1c1168c1b7d80b3f84c9d
Juan Moreno <jose.morenobrn@uanl.edu.mx>
2024-12-12 14:22:38 -0600

    feat: Add CRUD for Contacts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_12_175719) do
+ActiveRecord::Schema[8.0].define(version: 2024_12_12_191437) do
+  create_table "contacts", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.string "address"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_contacts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,4 +30,6 @@ ActiveRecord::Schema[8.0].define(version: 2024_12_12_175719) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "contacts", "users"
 end
